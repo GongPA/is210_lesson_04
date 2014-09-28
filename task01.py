03a83fdbc244a08bd4843263e79b9a21c8673be1
GongPA <david.chen@spsmail.cuny.edu>
2014-09-28 02:42:20 -0400

    homework04

---
+++ task01.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 1: Analyze a string
+    (1)Split into lines
+    (2)Split into works
+    (3)Using a nested loop
+"""
+
+import data
+print data.SHAKESPEARE
+MAXIMUM_WORDS = 0
+MINIMUM_WORDS = 0
+AVERAGE_WORDS = 0
+NUM_CRISPIAN = 0
+TOTAL_WORDS = 0
+LINES = data.SHAKESPEARE.split('\n') 
+for LINE in LINES:
+    WORDS = LINE.split()
+    WORDSIZE = len(WORDS)
+    TOTAL_WORDS += WORDSIZE
+    if WORDSIZE > MAXIMUM_WORDS:
+        MAXIMUM_WORDS = WORDSIZE
+    if WORDSIZE < MAXIMUM_WORDS:
+        MINIMUM_WORDS = WORDSIZE
+    if "Crispian" in LINE:
+        NUM_CRISPIAN += 1
+AVERAGE_WORDS = float(TOTAL_WORDS)/float(len(LINES))
+print MAXIMUM_WORDS, MINIMUM_WORDS, TOTAL_WORDS, AVERAGE_WORDS, NUM_CRISPIAN
